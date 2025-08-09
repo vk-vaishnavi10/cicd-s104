113101a35a76b33dd63bb648436d500b290f9e9c
vk-vaishnavi10 <kavanoorvaishnavi09@gmail.com>
2025-08-09 14:34:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-   <div>
-    <h1><i>this is CICD course</i></h1>
-    <p>CI/CD (Continuous Integration and Continuous Delivery/Deployment) is crucial for modern software development because it enables faster, more reliable, and more efficient software releases. By automating the build, test, and deployment processes, CI/CD minimizes manual errors, accelerates development cycles, and allows for more frequent updates and feature releases. 
-      This leads to quicker time to market, improved code quality, and enhanced collaboration among development teams. </p>
-      
+    <div>
+      <h1><i>this is CICD course</i></h1>
+      <p>
+        CI/CD (Continuous Integration and Continuous Delivery/Deployment) is crucial for modern software development because it enables faster, more reliable, and more efficient software releases. 
+        By automating the build, test, and deployment processes, CI/CD minimizes manual errors, accelerates development cycles, and allows for more frequent updates and feature releases. 
+        This leads to quicker time to market, improved code quality, and enhanced collaboration among development teams.
+      </p>
     </div>
   );
 }
